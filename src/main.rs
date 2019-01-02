1af9d94d63b784126b60bdd0eb8e3d9c4383209a
ryn1x <ryn1x@tutanota.com>
2019-01-02 00:58:04 -0700

    facored fn out into module

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,9 @@
+extern crate imgfilters;
 extern crate tiny_tiff;
 
+use imgfilters::median2d;
 use tiny_tiff::reader;
 use tiny_tiff::writer;
-
-fn median2d(img: Vec<Vec<f32>>, win_width: usize, win_height: usize) -> Vec<Vec<f32>> {
-    let mut out = img.clone();
-
-    let img_height = img[0].len();
-    let img_width = img.len();
-
-    let mut window = vec![0f32; win_width * win_height];
-
-    let edgex = win_width / 2;
-    let edgey = win_height / 2;
-
-    for x in edgex..(img_width - edgex) {
-        for y in edgey..(img_height - edgey) {
-            let mut i = 0;
-            for fx in 0..win_width {
-                for fy in 0..win_height {
-                    window[i] = img[x + fx - edgex][y + fy - edgey];
-                    i += 1;
-                }
-            }
-            window.sort_by(|a, b| a.partial_cmp(b).unwrap());
-            out[x][y] = window[win_width * win_height / 2];
-        }
-    }
-    out
-}
 
 fn main() {
     // read
